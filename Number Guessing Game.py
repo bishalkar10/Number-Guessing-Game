67234038f02d104f0fff13e6b7e2c66a8b84f3c4
Gain Prasad Kar <112653391+bishalkar10@users.noreply.github.com>
2023-01-24 22:54:11 +0530

    Changed the full Code
    Previous code has bugs. I have recreated the entire game...

--- Number Guessing Game.py
+++ Number Guessing Game.py
@@ -1,61 +1,100 @@
-import random
+import random # importing random module
 
-'''
-creating custom 
-Error below...
-'''
-
-class TooBigValueError(Exception):
+#creatimg custom Execptions 
+class BigValueError(Exception) : 
     pass 
-class TooSmallValueError(Exception):
+    
+class SmallValueError(Exception) : 
+    pass 
+
+class EmptyInputError(Exception) : 
     pass
+      
+# initiating value range     
+small_value = 1 
+big_value = 1000    
+number = random.randint(small_value, big_value)
 
-low_value = 1
-high_value = 1000
-number = random.randint(low_value, high_value)
 
-count = 0
-
-while True :
-    try :
-        guess = int (input("Enter Guess : "))
-        print ()
-        count += 1
-        
-        if guess > high_value :
-            raise TooBigValueError
-        
-        elif guess < low_value : 
-            raise TooSmallValueError
-        break 
-           
-    except ValueError :
-        print ('Please Enter a Number : ')
-        print ()  
+def play() :
+    print ("********New Game********")
+    print()
+    print (f"Enter a Value between {small_value} and {big_value}")   
+    print('Enter \'exit\' to exit the game')
+    print ()
     
-    except Exception : # this bolck of code is unnecessary 
-        print (Exception)
-        print ()
-        
-    except TooBigValueError :
-        print ('Value is too Big, try again! ', end = '')
-        print (f"Enter Value Between {low_value} & {high_value}.")
-        print ()
-        
-    except TooSmallValueError:
-        print ("Value is too small, try again! ", end ="")
-        print (f'Enter Value Between {low_value} & {high_value}.')
-        print ()
-        
-    if guess > number : 
-        print ("Enter a lower Value")
-        print ()
-        
-    elif guess < number : 
-        print ("Enter a Higher Value")
-        print ()
-        
-print ("You nailed it! ", end = "")
-print (f'You took {count} chances.')
-        
+    #Here we will count how many times user have guessed
+    count = 0
     
+    while True  : 
+        try : 
+            # taking input 
+            guess = input("Enter an Integer Number : ")
+            
+            # if user enter exit then exit the game
+            if guess.lower() == 'exit' : 
+                break 
+            # if it'a Empty input then raise EmptyInputError
+            if guess.strip() == '' : 
+                raise EmptyInputError
+
+            # convert to integer
+            guess = int(guess) 
+            
+            # if input is bigger than constraints then raise BigValueError              
+            if guess > big_value : 
+                raise BigValueError
+            
+            # if input is smaller than constraints then raise SmallValueError
+            if guess < small_value : 
+                raise SmallValueError 
+            
+            # if guess is correct then congrat User  
+            if guess == number :
+                count += 1 
+                print ()
+                print ("Congratulations! You Guessed it") 
+                print (f"You took {count} Chances")
+                print ()
+                
+            # ask him/her to play again
+                play_again = input("Do you want to play again?\nPress \'Y/y\' to play again : ")
+                
+                if play_again.lower() == 'y' : 
+                    play() 
+                else : 
+                      break
+             
+            elif small_value <= guess < number : 
+                count += 1
+                print ("Enter a Bigger Number") 
+                print () 
+            
+            elif big_value >= guess > number : 
+                count += 1
+                print ('Enter a Smaller Number') 
+                print ()
+            
+        # Handling Error s    
+        except EmptyInputError as e : 
+            print (f"You Didn't Enter anything. Enter valid Integer between {small_value} and {big_value}")  
+            print ()
+          
+        except BigValueError : 
+            print ("Your guess is bigger than original contraints. Enter a lower number") 
+            print () 
+        
+        except SmallValueError: 
+            print ("Your guess is smaller than original contraints. Enter a bigger number") 
+            print ()
+        
+        except ValueError : 
+            print ("Enter a Integer")
+            print()
+        
+        except Exception as e : 
+            print (e) 
+
+if __name__ == "__main__" :            
+    play()
+
